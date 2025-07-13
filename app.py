107a05b9de7c22dbaa41c32c1dca6ac4b4df80ba
youngChacha127 <ckdudrl0704@naver.com>
2025-07-13 16:27:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def get_sheet():
         'https://www.googleapis.com/auth/spreadsheets',
         'https://www.googleapis.com/auth/drive'
     ]
-    creds = Credentials.from_service_account_file('coupon-credentials.json', scopes=scope)
+    creds = Credentials.from_service_account_file('coupon-service-account-8c6fbed94166', scopes=scope)
     client = gspread.authorize(creds)
     sheet = client.open("coupon").sheet1
     return sheet
